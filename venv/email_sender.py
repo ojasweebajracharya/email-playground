a937bed76cef5b6e2b276e197a19e4eb204aea41
Ojaswee Professional <ojasweebajracharya@gmail.com>
2020-06-20 15:30:24 +0100

    Added a HTML file as the content of the email

--- venv/email_sender.py
+++ venv/email_sender.py
@@ -1,30 +1,30 @@
 import smtplib
-
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path # similar to os.path
 
-def sendEmail(sender, emailTo, emailSubject, emailContent, outlookEmail, outlookPassword):
-
+def sendEmail(sender, emailTo, emailSubject, outlookPassword):
+    html = Template(Path("index.html").read_text())
     email = EmailMessage()
 
     email["from"] = sender
     email["to"] = emailTo
     email["subject"] = emailSubject
 
-    email.set_content(emailContent)
+    email.set_content(html.substitute({"name":"Ojaswee"}), 'html')
 
     with smtplib.SMTP(host="smtp.outlook.com", port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login(outlookEmail, outlookPassword)
+        smtp.login(sender, outlookPassword)
         smtp.send_message(email)
         print("Its all good :D")
 
 sender = input("From: ")
-emailTo = input("Email to: ")
-emailSubject = input("Subjet: ")
-emailContent = input("Content of the email: ")
-
-outlookEmail = input("Enter your outlook email: ")
 outlookPassword = input("Enter your outlook password: ")
 
-sendEmail(sender, emailTo, emailSubject, emailContent, outlookEmail, outlookPassword)
+emailTo = input("Email to: ")
+emailSubject = input("Subject: ")
+
+
+sendEmail(sender, emailTo, emailSubject, outlookPassword)
